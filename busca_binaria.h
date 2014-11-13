f0bf701619754acbd0ce79d88b861d53f87c80d0
Diego Costa <diego@diegocosta.com.br>
2014-11-12 23:58:00 -0300

    Pequena correção nas assinaturas das funções

--- busca_binaria.h
+++ busca_binaria.h
@@ -5,5 +5,5 @@
 #include "busca_binaria.c"
 
 int buscabin_pesquisar(int v[],int tam,int valor,int *a);
-int buscabin_gerarnum(int v[],int tam);
-int buscabin_ordenar(int v[],int tam);
+void buscabin_gerarnum(int v[],int tam);
+void buscabin_ordenar(int v[],int tam);
